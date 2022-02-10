535b5f8e9e4dc9f481a04f12d049abb9a61c1a19
shani barak <shanibrk@gmail.com>
2022-02-10 17:32:57 +0200

    msg

---
+++ server/controllers/auth.js
@@ -0,0 +1,50 @@
+const {connect}= require('getstream');
+const bcrypt= require('bcrypt')
+const streamChat= require('stream-chat').StreamChat;
+const crypto= require('crypto')
+
+require('dotenv').config()
+
+const api_key=process.env.STREAM_API_KEY;
+const api_secret=process.env.STREAM_API_SECRET
+const app_id=process.env.STREAM_APP_ID;
+
+
+const signup=async (req,res)=>{
+    
+    try{
+        const {fullName, username, password, phoneNumber}= req.body;
+        const userId= crypto.randomBytes(16).toString('hex')
+
+        const serverClient= connect(api_key, api_secret, app_id)
+        const hashedPassword= await bcrypt.hash(password, 10)
+        const token= serverClient.createUserToken(userId)
+        res.status(200).send({token, fullName, username, userId, hashedPassword, phoneNumber})
+    }catch(error){
+        console.log(error)
+        res.status(500).send({message:error})
+    }
+}
+
+const login= async (req, res)=> {
+    try{
+        const {username, password}=req.body;
+        const serverClient= connect(api_key, api_secret, app_id)
+        const client= streamChat.getInstance(api_key, api_secret)
+        const {users}= await client.queryUsers({name:username})
+        if(!users.length) return res.status(400).send({message: 'User not found'})
+        const success= await bcrypt.compare(password, users[0].hashedPassword);
+        const token= serverClient.createUserToken(users[0].id)
+        if(success){
+            res.status(200).send({token, fullName: users[0].fullName, username, userId:users[0].id})
+        }else{
+            res.status(500).send({message:'Incorrect password'})
+        }
+    }catch(error){
+        console.log(error)
+        res.status(500).send({message:error})
+    }
+}
+
+
+module.exports={signup, login}
